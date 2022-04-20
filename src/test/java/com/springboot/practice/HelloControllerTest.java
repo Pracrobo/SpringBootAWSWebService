7e1ad5d545782af5571802e91e04eb54829861c5
Pracrobo <cpu180621@gmail.com>
2022-04-20 18:19:24 +0900

    HelloController, test code, lombok 작성 중..(p.74 dto method test 결과 검사중)

---
+++ src/test/java/com/springboot/practice/HelloControllerTest.java
@@ -0,0 +1,32 @@
+package com.springboot.practice;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.test.web.servlet.ResultMatcher;
+import web.HelloController;
+
+@ExtendWith(SpringExtension.class) //생략가능
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) content().string(hello));
+    }
+}
