ca6007a001119e10512599e59e42c5e013c38d8e
Pracrobo <cpu180621@gmail.com>
2022-04-28 10:08:55 +0900

    220428(목) 오전에 commit : JPA 연동진행중, 그동안의 날라간 부분 채움, 이해함(+4day)

--- src/test/java/com/springboot/practice/HelloControllerTest.java
+++ src/test/java/com/springboot/practice/HelloControllerTest.java
@@ -1,6 +1,8 @@
 package com.springboot.practice;
 
 
+import com.springboot.practice.web.HelloController;
+import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +10,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.springframework.test.web.servlet.ResultMatcher;
-import web.HelloController;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class) //생략가능
 @WebMvcTest(controllers = HelloController.class)
@@ -27,6 +25,19 @@ public class HelloControllerTest {
 
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
-                .andExpect((ResultMatcher) content().string(hello));
+                .andExpect(content().string(hello));
     }
+
+    @Test
+    public void helloDto가_리턴한다() throws Exception{
+        String name ="hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", Is.is(name)))
+                .andExpect(jsonPath("$.amount", Is.is(amount)));
+    }
+
 }
