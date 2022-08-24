fedbc471ebeabea734dc7a8fadd87cb09b140313
Pracrobo <cpu180621@gmail.com>
2022-08-24 21:46:59 +0900

    22/08/24 p.97 test차례

---
+++ src/main/java/com/springboot/practice/domain/posts/Posts.java
@@ -0,0 +1,34 @@
+package com.springboot.practice.domain.posts;
+
+import lombok.Builder;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+
+@Getter
+@NoArgsConstructor
+@EntityScan
+public class Posts {
+    @Id
+    @Generated()
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
--- src/test/java/com/springboot/practice/HelloControllerTest.java
+++ src/test/java/com/springboot/practice/HelloControllerTest.java
@@ -7,16 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.springframework.test.web.servlet.ResultMatcher;
-import web.HelloController;
-
 @ExtendWith(SpringExtension.class) //생략가능
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloControllerTest.class)
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
